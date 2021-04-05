6fefa80117342802d8cc64b10d0d948b03a5e297
Diogo Domingos <diogo_goulart@hotmail.com>
2021-04-05 00:30:36 -0300

    Rails part 1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get "produtos", to: "produtos#index"
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
+  resources :produtos, only: [:new, :create, :destroy]
+  #delete "produtos/:id/remover", to: "produtos#destroy", as: :produto  
+  #get "produtos/new", to:"produtos#new"
+  #post "produtos", to: "produtos#create"
+  root to: "produtos#index"
 end
